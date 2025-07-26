61d758ee49278a052855628403464bd63f47ea65
sidpd51 <sidpd510@gmail.com>
2025-07-26 16:02:35 +0530

    feat: Add redis env variables and implemente connectToRedis method

---
+++ server/src/config/redis.config.ts
@@ -0,0 +1,26 @@
+import Redis from "ioredis";
+import { serverConfig } from ".";
+
+function connectToRedis() {
+    try {
+        let connection: Redis;
+        const redisConfig = {
+            host: serverConfig.REDIS_HOST,
+            port: serverConfig.REDIS_PORT,
+            password: serverConfig.REDIS_PASSWORD,
+            maxRetriesPerRequest: null,
+        }
+        return () => {
+            if (!connection) {
+                connection = new Redis(redisConfig);
+                return connection;
+            }
+            return connection;
+        }
+    } catch (error) {
+        console.error("Error connecting to Redis:", error);
+        throw error;
+    }
+}
+
+export const getRedisClient = connectToRedis();
