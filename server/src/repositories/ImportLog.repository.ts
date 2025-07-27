8eaa221e334d33b90384ad9a652672330c1adda0
sidpd51 <sidpd510@gmail.com>
2025-07-28 01:55:27 +0530

    feat: implement pagination and metadata for getAllImportLogs function

--- server/src/repositories/ImportLog.repository.ts
+++ server/src/repositories/ImportLog.repository.ts
@@ -11,7 +11,35 @@ export const createImportLog = async (paylod: Partial<IImportLog>) => {
     }
 }
 
-export const getAllImportLogs = async () => {
-    const logs = await ImportLog.find({});
-    return logs;
+export const getAllImportLogs = async (page = 1, limit = 10) => {
+    const skip = (page - 1) * limit;
+
+    const [logs, totalCount] = await Promise.all([
+        ImportLog.aggregate([
+            { $sort: { timestamp: -1 } },
+            { $skip: skip },
+            { $limit: limit },
+            {
+                $project: {
+                    fileName: 1,
+                    timestamp: 1,
+                    totalFetched: 1,
+                    totalImported: 1,
+                    newJobs: 1,
+                    updatedJobs: 1,
+                    failedJobs: { $size: { $ifNull: ["$failedJobs", []] } }
+                }
+            }
+        ]),
+        ImportLog.countDocuments()
+    ]);
+
+    return {
+        data: logs,
+        meta: {
+            totalCount,
+            currentPage: page,
+            totalPages: Math.ceil(totalCount / limit),
+        },
+    };
 }
