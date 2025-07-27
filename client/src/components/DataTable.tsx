8dbc7e84893f8062ad2146e6c043f898acd09445
sidpd51 <sidpd510@gmail.com>
2025-07-28 02:55:53 +0530

    feat: add DataTable component for displaying import logs and update pagination limit in controller

---
+++ client/src/components/DataTable.tsx
@@ -0,0 +1,78 @@
+import {
+    type ColumnDef,
+    flexRender,
+    getCoreRowModel,
+    getSortedRowModel,
+    type SortingState,
+    useReactTable,
+} from '@tanstack/react-table';
+import { useState } from 'react';
+import type { IData } from '../hooks/useImportLog';
+
+interface DataTableProps {
+    data: IData[];
+}
+
+const DataTable: React.FC<DataTableProps> = ({ data }) => {
+    const [sorting, setSorting] = useState<SortingState>([]);
+    
+
+    const columns: ColumnDef<IData>[] = [
+        { accessorKey: 'fileName', header: 'file Name' },
+        { accessorKey: 'timestamp', header: 'Import DateTime' },
+        { accessorKey: 'totalImported', header: 'Total' },
+        { accessorKey: 'newJobs', header: 'New' },
+        { accessorKey: 'updatedJobs', header: 'Updated' },
+        { accessorKey: 'failedJobs', header: 'Failed' },
+    ];
+
+    const table = useReactTable({
+        data,
+        columns,
+        state: {
+            sorting,
+        },
+        onSortingChange: setSorting,
+        getCoreRowModel: getCoreRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+    });
+
+    return (
+        <div className="overflow-x-auto" data-theme="retro">
+            <table className="table table-lg">
+                <thead className=''>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <tr key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => (
+                                <th
+                                    key={header.id}
+                                    onClick={header.column.getToggleSortingHandler()}
+                                    className="px-4 py-2 text-left text-sm font-medium text-gray-700 border-b cursor-pointer select-none"
+                                >
+                                    {flexRender(header.column.columnDef.header, header.getContext())}
+                                    {{
+                                        asc: ' 🔼',
+                                        desc: ' 🔽',
+                                    }[header.column.getIsSorted() as string] ?? null}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody>
+                    {table.getRowModel().rows.map((row) => (
+                        <tr key={row.id} className="hover:bg-gray-50">
+                            {row.getVisibleCells().map((cell) => (
+                                <td key={cell.id} className="px-4 py-2 text-sm text-gray-600 border-b">
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default DataTable;
