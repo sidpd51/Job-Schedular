b74a5faba914fd762ce12534743c7dd954acc505
sidpd51 <sidpd510@gmail.com>
2025-08-06 21:50:05 +0530

    feat: add shadcn and configure for project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from "./components/ui/button"
 
 function App() {
 
   return (
-    <h1>hello world</h1>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button onClick={()=>{console.log("button clicked")}}>Click me</Button>
+    </div>
   )
 }
 
